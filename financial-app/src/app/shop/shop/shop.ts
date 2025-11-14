15a3fd64f7cebc28cefb1b9fb8079017d1ca20ab
bahar.lawter@gmail.com <baharlawter@gmail.com>
2025-11-14 11:35:19 -0600

    added couple of books with images in shop component

--- financial-app/src/app/shop/shop/shop.ts
+++ financial-app/src/app/shop/shop/shop.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'shop',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './shop.html',
   styleUrl: './shop.scss',
 })
-export class Shop {}
+export class Shop {
+  books = [
+    {
+      title: 'The Total Money Makeover Updated and Expanded: A Proven Plan for Financial Peace',
+      image: 'https://m.media-amazon.com/images/I/81RvIwENAbL._SY522_.jpg',
+      price: '$24.20',
+    },
+    {
+      title:
+        'Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do',
+      image: 'https://m.media-amazon.com/images/I/81BE7eeKzAL._SY522_.jpg',
+      price: '$7.34',
+    },
+  ];
+}
