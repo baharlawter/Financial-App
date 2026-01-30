0777cd0a56f484604a01613d6aa8d60c5734c078
bahar.lawter@gmail.com <baharlawter@gmail.com>
2026-01-30 08:43:32 -0600

    it is working so far. Calculator page is nicely working

--- Front-End/FrontEnd/src/App.jsx
+++ Front-End/FrontEnd/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import MortgageCalculator from "./MortgageCalculator/MortgageCalculator";
+import Header from "./Header/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      MortgageCalculator
       <p>Hello</p>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<p>Hello</p>} />
+        <Route path="/calculator" element={<MortgageCalculator />} />
+      </Routes>
     </>
   );
 }
--- Front-End/FrontEnd/src/MortgageCalculator/MortgageCalculator.jsx
+++ Front-End/FrontEnd/src/MortgageCalculator/MortgageCalculator.jsx
@@ -1,8 +1,8 @@
 function MortgageCalculator() {
   return (
     <>
-      const r = annualRate / 100 / 12; const n = years * 12; return (principal *
-      (r * Math.pow(1 + r, n))) / (Math.pow(1 + r, n) - 1);
+      {/* const r = annualRate / 100 / 12; const n = years * 12; return (principal *
+      (r * Math.pow(1 + r, n))) / (Math.pow(1 + r, n) - 1); */}
       <input type="number" placeholder="Loan Amount" />
       <input type="number" placeholder="Interest Rate (%)" />
       <input type="number" placeholder="Years" />
