e563f902b3d0049951ecf677ffbd0412dc792991
bahar.lawter@gmail.com <baharlawter@gmail.com>
2026-01-30 08:15:08 -0600

    created the formula for mortgage calculator

--- Front-End/FrontEnd/src/App.jsx
+++ Front-End/FrontEnd/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import MortgageCalculator from "./MortgageCalculator/MortgageCalculator";
 
 function App() {
   return (
     <>
+      MortgageCalculator
       <p>Hello</p>
     </>
   );
---
+++ Front-End/FrontEnd/src/MortgageCalculator/MortgageCalculator.jsx
@@ -0,0 +1,13 @@
+function MortgageCalculator() {
+  return (
+    <>
+      const r = annualRate / 100 / 12; const n = years * 12; return (principal *
+      (r * Math.pow(1 + r, n))) / (Math.pow(1 + r, n) - 1);
+      <input type="number" placeholder="Loan Amount" />
+      <input type="number" placeholder="Interest Rate (%)" />
+      <input type="number" placeholder="Years" />
+      <button>Calculate</button>
+    </>
+  );
+}
+export default MortgageCalculator;
