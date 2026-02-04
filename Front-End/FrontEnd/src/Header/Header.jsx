b672be036ef0756296fea21815c2e85e7c6ee74e
bahar.lawter@gmail.com <baharlawter@gmail.com>
2026-02-04 10:06:02 -0600

    created cards forlder and file

--- Front-End/FrontEnd/src/Header/Header.jsx
+++ Front-End/FrontEnd/src/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header>
+      <h1>Financial App</h1>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/calculator">Mortgage Calculator</Link>
