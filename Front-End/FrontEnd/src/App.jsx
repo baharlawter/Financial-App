7e65c65dc3ba9c47deb681471d69468bda8b8ba1
bahar.lawter@gmail.com <baharlawter@gmail.com>
2026-02-10 08:45:06 -0600

    updated path and the header

--- Front-End/FrontEnd/src/App.jsx
+++ Front-End/FrontEnd/src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import MortgageCalculator from "./MortgageCalculator/MortgageCalculator";
 import Header from "./Header/Header";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <p>Hello</p>
+    <BrowserRouter>
       <Header />
 
       <Routes>
-        <Route path="/" element={<p>Hello</p>} />
-        <Route path="/calculator" element={<MortgageCalculator />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
--- Front-End/FrontEnd/src/Header/Header.jsx
+++ Front-End/FrontEnd/src/Header/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
-
+import "./Header.css";
 function Header() {
   return (
-    <header>
-      <h1>Financial App</h1>
-      <nav>
+    <header className="header">
+      <h1 className="logo">Financial App</h1>
+      <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/calculator">Mortgage Calculator</Link>
+        <Link to="/search">Search</Link>
       </nav>
     </header>
   );
