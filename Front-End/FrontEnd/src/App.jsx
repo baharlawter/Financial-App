5bc2b128721f62c2407af41cbfc8481cf39ab3f0
bahar.lawter@gmail.com <baharlawter@gmail.com>
2026-02-10 08:53:02 -0600

    the header is working but it is in the middle of the page

--- Front-End/FrontEnd/src/App.jsx
+++ Front-End/FrontEnd/src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import MortgageCalculator from "./MortgageCalculator/MortgageCalculator";
 import Header from "./Header/Header";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/mortgage-calculator" element={<MortgageCalculator />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
