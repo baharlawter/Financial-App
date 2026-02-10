cfecf2d15442219d400b749b1fec530927ab5db8
bahar.lawter@gmail.com <baharlawter@gmail.com>
2026-02-10 10:28:42 -0600

    created cards jsx for reusable cards

--- Front-End/FrontEnd/src/Cards/Cards.jsx
+++ Front-End/FrontEnd/src/Cards/Cards.jsx
@@ -2,21 +2,14 @@ import { Link } from "react-router-dom";
 import "./Cards.css";
 
 export default function Cards() {
-  const items = [
-    { title: "Get Out of Debt", link: "/debt" },
-    { title: "Buy Real Estate", link: "/real-estate" },
-    { title: "Create a Budget", link: "/budget" },
-    { title: "Save for retirement", link: "retirment" },
-  ];
-
+  // let cards = [
+  //   { "Pay off Your House Early": "link" },
+  //   { "How to Pay off your credit Card": "link" },
+  //   { "Invest in Real Estate": "Link" },
+  // ];
   return (
-    <div className="card-grid">
-      {items.map((item) => (
-        <Link to={item.link} className="card" key={item.title}>
-          <div className="icon">{item.icon}</div>
-          <h3>{item.title}</h3>
-        </Link>
-      ))}
-    </div>
+    <>
+      <div>cards.map()</div>
+    </>
   );
 }
