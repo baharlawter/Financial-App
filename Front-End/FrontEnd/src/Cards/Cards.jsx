201b7b3d81069b210e8f4b7b1c5c84242cbc42a9
bahar.lawter@gmail.com <baharlawter@gmail.com>
2026-02-11 08:57:41 -0600

    Cards page is created but not working

--- Front-End/FrontEnd/src/Cards/Cards.jsx
+++ Front-End/FrontEnd/src/Cards/Cards.jsx
@@ -2,14 +2,20 @@ import { Link } from "react-router-dom";
 import "./Cards.css";
 
 export default function Cards() {
-  // let cards = [
-  //   { "Pay off Your House Early": "link" },
-  //   { "How to Pay off your credit Card": "link" },
-  //   { "Invest in Real Estate": "Link" },
-  // ];
+  const cards = [
+    { title: "Card 1", img: "link1", par: "some par" },
+    { title: "Card 2", img: "link2", par: "some par" },
+    { title: "Card 3", img: "link3", par: "some par" },
+  ];
   return (
     <>
-      <div>cards.map()</div>
+      return ({" "}
+      <div style={{ display: "flex", gap: "20px" }}>
+        {" "}
+        {cards.map((c, index) => (
+          <Card key={index} title={c.title} img={c.img} par={c.par} />
+        ))}{" "}
+      </div>
     </>
   );
 }
